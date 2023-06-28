8a8f0b33fbd5ccae6bd1d485b3c2c6d0de03711a
edoch <edo_chio@hotmail.it>
2023-06-28 09:27:04 +0200

    tests on custom data

---
+++ models/example_text.py
@@ -0,0 +1,117 @@
+TEXT = """
+Informazioni sull offerta di lavoro
+
+Leonardo, azienda globale ad alta tecnologia, è tra le prime società al mondo che operano nel settore dell Aerospazio, Difesa e Sicurezza.
+
+
+Leonardo ha una rilevante presenza industriale in Italia, Regno Unito, Polonia e USA anche attraverso società controllate, joint venture e partecipazioni di respiro internazionale.
+
+
+L azienda compete sui più importanti mercati internazionali attraverso le proprie aree di business: Elicotteri, Velivoli, Aerostrutture, Elettronica, Cyber & Security e Spazio.
+
+Leonardo è uno dei tre partner strategici del programma GCAP (Global Combat Air Programme) per lo sviluppo del fighter di sesta generazione, finalizzato alla realizzazione, insieme a UK e Giappone, del “sistema di sistemi” per operazioni multi-dominio in ambito difesa.
+
+
+"""
+
+TEXT_2 = """
+GCAP rappresenta uno dei programmi più sfidanti e avveniristici per l\'industria dell\'aerospazio e della difesa perché guiderà la rivoluzione tecnologica che caratterizzerà il settore nei prossimi cinquant\'anni.
+
+
+Una sfida finalizzata a rafforzare la sovranità tecnologica e industriale dei paesi coinvolti, perché punta a identificare e a rendere disponibili quelle tecnologie innovative, dette abilitanti, che assicureranno il salto generazionale, generando ritorni positivi e progresso economico e sociale per l\'intero sistema Paese, a beneficio delle nuove generazioni.
+
+
+Le risorse che lavoreranno all\'interno di questo programma avranno l\'opportunità di accedere ad un percorso di crescita professionale in un contesto internazionale e tecnologicamente avanzato.
+"""
+
+TEXT_ORIG = """
+Informazioni sull\'offerta di lavoro
+
+Leonardo, azienda globale ad alta tecnologia, è tra le prime società al mondo che operano nel settore dell\'Aerospazio, Difesa e Sicurezza.
+
+
+Leonardo ha una rilevante presenza industriale in Italia, Regno Unito, Polonia e USA anche attraverso società controllate, joint venture e partecipazioni di respiro internazionale.
+
+
+L\'azienda compete sui più importanti mercati internazionali attraverso le proprie aree di business: Elicotteri, Velivoli, Aerostrutture, Elettronica, Cyber & Security e Spazio.
+
+
+Leonardo è uno dei tre partner strategici del programma GCAP (Global Combat Air Programme) per lo sviluppo del fighter di sesta generazione, finalizzato alla realizzazione, insieme a UK e Giappone, del “sistema di sistemi” per operazioni multi-dominio in ambito difesa.
+
+
+GCAP rappresenta uno dei programmi più sfidanti e avveniristici per l\'industria dell\'aerospazio e della difesa perché guiderà la rivoluzione tecnologica che caratterizzerà il settore nei prossimi cinquant\'anni.
+
+
+Una sfida finalizzata a rafforzare la sovranità tecnologica e industriale dei paesi coinvolti, perché punta a identificare e a rendere disponibili quelle tecnologie innovative, dette abilitanti, che assicureranno il salto generazionale, generando ritorni positivi e progresso economico e sociale per l\'intero sistema Paese, a beneficio delle nuove generazioni.
+
+
+Le risorse che lavoreranno all\'interno di questo programma avranno l\'opportunità di accedere ad un percorso di crescita professionale in un contesto internazionale e tecnologicamente avanzato.
+
+
+Stiamo ricercando un/a AI Engineer
+
+
+SEDE: Torino, Corso Francia
+
+La risorsa verrà inserita nell\'ambito dell\'unità Avionics Systems ed in particolare nella disciplina dei Sviluppo SW Avionico e dovrà essere in grado di contribuire alle attività di sviluppo di algoritmi di AI da applicare a velivoli unmanned e di definire metodolgie per la loro validazione in campo avionico.
+
+
+La risorsa si occuperà principalmente di:
+
+
+· Fornire competenze in materia nei settori dell'Intelligenza Artificiale (AI), Machine Learning (ML) e Deep Learning (DL)
+
+· Sviluppare nuovi algoritmi e soluzioni di intelligenza artificiale in ambito avionico
+
+· Refactoring di soluzioni già esistenti create utilizzando algoritmi standard, con l\'obiettivo di renderle più efficaci utilizzando ML/DL
+
+· Sviluppare e addestrare modelli ML/DL; identificare e compensare bias e overfitting
+
+· Identificare nuovi approcci di verifica e validazione di applicazioni AI/ML/DL secondo le normative vigenti in campo avionico
+
+· Scrivere specifiche, requisiti e documentazione
+
+
+La/il candidata/o ideale offre la propria disponibilità a svolgere l\'attività lavorativa principalmente in sede con possibilità di trasferte all\'estero anche in aree extra-UE
+
+
+Titolo di studio: preferenza per Laurea magistrale in informatica, matematica, ingegneria o settore tecnico correlato.
+
+
+Livello di esperienza richiesto: 2+ anni di esperienza lavorativa in ambito Machine Learning e/o Deep Learning
+
+
+HARD SKILLS
+
+
+    Esperienza pregressa nella creazione di pipeline ML/DL e nell'addestramento e nella convalida di modelli ML/DL
+    Buona conoscenza di tecniche e modelli di apprendimento supervisionato e non supervisionato, reti neurali (convoluzionali, ricorrenti, ecc.) e tecniche di apprendimento (deep learning e transfer learning)
+    Buona conoscenza di Python e principali librerie (es. Numpy, Pandas, Seaborn, Matplotlib/Plotly)
+    Buona conoscenza di Tensorflow. Apprezzata la conoscenza di PyTorc
+
+
+NICE TO HAVE
+
+
+    Esperienza con C/C++
+    Esperienza con architetture containerizzate (Docker e Kubernetes)
+    Esperienza precedente in campo avionico
+
+
+SOFT SKILLS
+
+
+    Capacità di lavorare in team
+    Capacità di relazionarsi con differenti culture
+    Capacità di comunicazione e di relazione con clienti e con i fornitori
+    Capacità organizzative e di coordinamento
+    Capacità di pianificazione
+    Problem solving ed orientamento al risultato
+
+
+
+Conoscenze linguistiche:
+
+
+Ottima conoscenza dell\'inglese scritto e orale
+"""
--- models/falcon_model.py
+++ models/falcon_model.py
@@ -7,40 +7,94 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.document_loaders import YoutubeLoader
 import textwrap
 
-# --------------------------------------------------------------
+#%%
 # Load the HuggingFaceHub API token from the .env file
 # --------------------------------------------------------------
-
 load_dotenv(find_dotenv())
 HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
 
 
+# %%
+# Load the text
 # --------------------------------------------------------------
+from example_text import TEXT_ORIG
+text_input=TEXT_ORIG
+print(len(text_input.split(' ')))
+
+
+#%%
 # Load the LLM model from the HuggingFaceHub
 # --------------------------------------------------------------
-
-repo_id = "tiiuae/falcon-7b-instruct"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+repo_id = "tiiuae/falcon-7b"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
 falcon_llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 500}
-)
+    repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 1000}
+) # type: ignore
 
-
+#%%
+# Load an OpenAI model for comparison
 # --------------------------------------------------------------
+openai_llm = OpenAI(
+    model_name="text-davinci-003", temperature=0.1, max_tokens=1000 # type: ignore
+)  # max token length is 4097
+
+
+#%%
 # Create a PromptTemplate and LLMChain
 # --------------------------------------------------------------
-template = """Question: {question}
+template_translation = """User: translate in english the text included between < and > in English
 
-Answer: Let's think step by step."""
+<{text}>
 
-prompt = PromptTemplate(template=template, input_variables=["question"])
+Assistant: I present you the translation in english, an nothing else, of the provided text
+"""
+
+#%%
+# Run the LLMChain with Falcon7b: translate the text
+# --------------------------------------------------------------
+prompt = PromptTemplate(template=template_translation, input_variables=["text"])
 llm_chain = LLMChain(prompt=prompt, llm=falcon_llm)
 
+question = f"""
+{text_input}
+"""
+response = llm_chain.run(question)
+translated_text = textwrap.fill(
+    response, width=100, break_long_words=False, replace_whitespace=False
+)
+print(translated_text)
 
+#%%
+# Run the LLMChain with Falcon7b: understand the text
 # --------------------------------------------------------------
-# Run the LLMChain
-# --------------------------------------------------------------
+template_extraction="""User: I need you to extract the soft skills from the job described in the text included between < and >
 
-question = "How do I make a sandwich?"
+<{text}>
+
+Assistant: The Soft skills are
+"""
+
+prompt = PromptTemplate(template=template_extraction, input_variables=["text"])
+llm_chain = LLMChain(prompt=prompt, llm=falcon_llm)
+
+question = f"""
+{translated_text}
+"""
+response = llm_chain.run(question)
+wrapped_text = textwrap.fill(
+    response, width=100, break_long_words=False, replace_whitespace=False
+)
+print(wrapped_text)
+
+#%%
+# Run the LLMChain with OpenAI
+# --------------------------------------------------------------
+#%%
+prompt = PromptTemplate(template=template_extraction, input_variables=["text"])
+llm_chain = LLMChain(prompt=prompt, llm=openai_llm)
+
+question = f"""
+{translated_text}
+"""
 response = llm_chain.run(question)
 wrapped_text = textwrap.fill(
     response, width=100, break_long_words=False, replace_whitespace=False
@@ -48,21 +102,43 @@ wrapped_text = textwrap.fill(
 print(wrapped_text)
 
 
-# --------------------------------------------------------------
-# Load a video transcript from YouTube
-# --------------------------------------------------------------
 
-video_url = "https://www.youtube.com/watch?v=riXpu1tHzl0"
-loader = YoutubeLoader.from_youtube_url(video_url)
-transcript = loader.load()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+
+# --------------------------------------------------------------
+# Split text
+# -------------------------------------------------------------
+#%%
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000)
-docs = text_splitter.split_documents(transcript)
+docs = text_splitter.split_text(TEXT)
 
 # --------------------------------------------------------------
 # Summarization with LangChain
 # --------------------------------------------------------------
-
+#%%
 # Add map_prompt and combine_prompt to the chain for custom summarization
 chain = load_summarize_chain(falcon_llm, chain_type="map_reduce", verbose=True)
 print(chain.llm_chain.prompt.template)
@@ -71,7 +147,7 @@ print(chain.combine_document_chain.llm_chain.prompt.template)
 # --------------------------------------------------------------
 # Test the Falcon model with text summarization
 # --------------------------------------------------------------
-
+#%%
 output_summary = chain.run(docs)
 wrapped_text = textwrap.fill(
     output_summary, width=100, break_long_words=False, replace_whitespace=False
@@ -82,7 +158,7 @@ print(wrapped_text)
 # --------------------------------------------------------------
 # Load an OpenAI model for comparison
 # --------------------------------------------------------------
-
+#%%
 openai_llm = OpenAI(
     model_name="text-davinci-003", temperature=0.1, max_tokens=500
 )  # max token length is 4097
